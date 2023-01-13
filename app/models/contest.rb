2cc88efb1368fc96005812f149d67bae4307566c
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-01-13 20:52:26 +0100

    Made validations to all the fields in Contest model

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,3 +1,9 @@
 class Contest < ApplicationRecord
-  validates :email, uniqueness: { message: "Email already exist." }
+  before_save {self.email = email.downcase}
+  validates :email, presence: true, uniqueness: { message: "Email already exist." },
+                                  # Verify email with ruby's standard library regex.
+                                  format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :slogan, presence: true, length: { maximum: 50 }
 end
