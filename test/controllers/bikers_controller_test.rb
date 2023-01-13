08aa1b714eb57568b74b351da8db469573d74027
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-01-13 20:02:26 +0100

    Performed rails testing for bikers index action

--- test/controllers/bikers_controller_test.rb
+++ test/controllers/bikers_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class BikersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get api_v1_bikers_url
+    assert_response :success
+  end
 end
